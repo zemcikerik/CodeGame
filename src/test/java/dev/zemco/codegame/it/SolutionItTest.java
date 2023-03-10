d28d5eba8533fb127045b0f77aaeffaafed711cb
Erik Zemčík <zemcik.erik@gmail.com>
2023-03-10 08:06:37 +0100

    Add test evaluation integration tests

--- src/test/java/dev/zemco/codegame/it/SolutionItTest.java
+++ src/test/java/dev/zemco/codegame/it/SolutionItTest.java
@@ -13,6 +13,7 @@ import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.control.TextFlowMatchers;
 
 import static dev.zemco.codegame.TestConstants.INTEGRATION_TEST;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.testfx.api.FxAssert.verifyThat;
@@ -21,6 +22,7 @@ import static org.testfx.matcher.base.NodeMatchers.isEnabled;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
 import static org.testfx.matcher.base.WindowMatchers.isFocused;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
+import static org.testfx.matcher.control.TableViewMatchers.containsRow;
 
 @Tag(INTEGRATION_TEST)
 @ExtendWith(ApplicationExtension.class)
@@ -35,12 +37,17 @@ public class SolutionItTest {
     private static final String STEP = "#stepButton";
     private static final String BACK = "#backButton";
 
+    private static final String NEXT_INSTRUCTION_LINE = ".next-instruction-line";
+    private static final String MEMORY_STATE_TABLE = "#memoryView";
+    private static final String EXECUTION_ERROR = ".execution-error";
+
     private static final String DIALOG_TEXT = ".dialog-pane .content";
     private static final String DIALOG_CLOSE = ".dialog-pane .button";
     private static final String SYNTAX_ERROR_TITLE = "Syntax Error";
     private static final String SYNTAX_ERROR = ".syntax-error";
     private static final String EVALUATION_ERROR_TITLE = "Evaluation Failure";
     private static final String SUBMISSION_SUCCESS_TITLE = "Submission Success";
+    private static final String EXECUTION_ERROR_TITLE = "Execution Error";
 
     private static final String PROBLEM_LIST = "#problemListView";
     private static final String VALID_SOLUTION = ">loop\nin\nout\njump loop";
@@ -130,6 +137,68 @@ public class SolutionItTest {
         robot.clickOn(TOGGLE_EVALUATION);
         verifyThat(BACK, isDisabled());
         verifyThat(STEP, isEnabled());
+    }
+
+    @Test
+    public void testEvaluationCanBeStepped(FxRobot robot) {
+        robot.clickOn(CODE_AREA).write(VALID_SOLUTION);
+        robot.clickOn(COMPILE).clickOn(TOGGLE_EVALUATION);
+
+        verifyThat(NEXT_INSTRUCTION_LINE, TextFlowMatchers.hasText("in"));
+        robot.clickOn(STEP);
+        verifyThat(NEXT_INSTRUCTION_LINE, TextFlowMatchers.hasText("out"));
+        robot.clickOn(STEP);
+        verifyThat(NEXT_INSTRUCTION_LINE, TextFlowMatchers.hasText("jump loop"));
+    }
+
+    @Test
+    public void testEvaluationShouldDisplayMemoryStateOfUnderlyingExecution(FxRobot robot) {
+        robot.clickOn(CODE_AREA).write(VALID_SOLUTION);
+        robot.clickOn(COMPILE).clickOn(TOGGLE_EVALUATION);
+
+        verifyThat(MEMORY_STATE_TABLE, containsRow("Working Cell", "-"));
+        robot.clickOn(STEP);
+        verifyThat(MEMORY_STATE_TABLE, containsRow("Working Cell", "1"));
+    }
+
+    @Test
+    public void testEvaluationShouldDisplayErrorInfoIfUnderlyingExecutionFails(FxRobot robot) {
+        robot.clickOn(CODE_AREA).write("out");
+        robot.clickOn(COMPILE).clickOn(TOGGLE_EVALUATION);
+        robot.clickOn(STEP);
+
+        verifyThat(robot.window(EXECUTION_ERROR_TITLE), isFocused());
+        verifyThat(DIALOG_TEXT, hasText(allOf(
+            containsString("execution on line 1!"),
+            containsString("Working cell does not hold value!")
+        )));
+        robot.clickOn(DIALOG_CLOSE);
+
+        verifyThat(EXECUTION_ERROR, TextFlowMatchers.hasText("out"));
+        verifyThat(STEP, isDisabled());
+        verifyThat(SUBMIT, isDisabled());
+    }
+
+    @Test
+    public void testEvaluationShouldDisplayErrorInfoWhenNoNextInstructionAvailable(FxRobot robot) {
+        robot.clickOn(COMPILE).clickOn(TOGGLE_EVALUATION);
+        robot.clickOn(STEP);
+
+        verifyThat(robot.window(EXECUTION_ERROR_TITLE), isFocused());
+        verifyThat(DIALOG_TEXT, hasText(containsString("No next instruction")));
+        robot.clickOn(DIALOG_CLOSE);
+
+        verifyThat(STEP, isDisabled());
+        verifyThat(SUBMIT, isDisabled());
+    }
+
+    @Test
+    public void testEvaluationShouldHandleMemoryManipulation(FxRobot robot) {
+        robot.clickOn(CODE_AREA).write("load 2\nadd 12\nsave 1");
+        robot.clickOn(COMPILE).clickOn(TOGGLE_EVALUATION);
+        robot.clickOn(STEP).clickOn(STEP).clickOn(STEP);
+
+        verifyThat(MEMORY_STATE_TABLE, containsRow("1", "-99988"));
     }
 
 }
